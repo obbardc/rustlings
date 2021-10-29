dd311038a68ef1e54b4a572e915e463999947b6a
Christopher Obbard <chris.obbard@collabora.com>
2021-10-29 12:21:27 +0100

    progress.
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,11 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Move(Point),
+    Echo(String),
+    ChangeColor((u8, u8, u8)),
+    Quit,
 }
 
 struct Point {
@@ -36,7 +37,13 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
+        // create a match expression to process the different message variants
+        match message {
+            Message::Move(position) => self.move_position(position),
+            Message::Echo(s) => self.echo(s),
+            Message::ChangeColor((a, b, c)) => self.change_color((a, b, c)),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
