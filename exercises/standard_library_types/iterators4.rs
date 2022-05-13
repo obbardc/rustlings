58f9b50589fe6b552e524505bfb3904145460cbc
Christopher Obbard <chris.obbard@collabora.com>
2022-05-13 10:19:58 +0100

    today work
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,17 +1,16 @@
 // iterators4.rs
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
-    // Complete this function to return the factorial of num
-    // Do not use:
-    // - return
-    // Try not to use:
-    // - imperative style loops (for, while)
-    // - additional variables
-    // For an extra challenge, don't use:
-    // - recursion
-    // Execute `rustlings hint iterators4` for hints.
+    // Not sure this is correct; but it works
+    let mut iter = 1..num + 1;
+    let mut fact = 1;
+    loop {
+        match iter.next() {
+            Some(x) => fact = fact * x,
+            None => break,
+        }
+    }
+    fact
 }
 
 #[cfg(test)]
